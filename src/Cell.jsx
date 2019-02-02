6c3da6b1b874ba0a2e07deddc05146aae41e0c48
HugoCapocci <h.capocci@gmail.com>
2019-02-02 16:13:12 +0100

    step3 - click on cell to change value

--- src/Cell.jsx
+++ src/Cell.jsx
@@ -3,11 +3,17 @@ import React, { Component } from 'react';
 import './Cell.css';
 
 export default class Cell extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
 
   render() {
     return (
-      <button className="cell">
-        {this.props.value}
+      <button className="cell" onClick={() => { this.setState({value: 'X'}); }}>
+        {this.state.value}
       </button>
     );
   }
