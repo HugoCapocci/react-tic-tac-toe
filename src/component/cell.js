86cb98765c967f18c75b44cef6b8f55da233c2f0
Arinouchkine <arinouchkine@gmail.com>
2019-02-05 14:27:54 +0100

    third step

--- src/component/cell.js
+++ src/component/cell.js
@@ -2,9 +2,20 @@ import React, { Component } from 'react';
 import Cell from "./grille";
 
 export default class Grille extends Component {
+
+    state = {
+        InValue : null,
+    }
+
+
+    AffP = () => {
+       this.setState({InValue:"X"});
+    }
+
     render() {
         return (
-            <button className='cell'>{this.props.value}</button>
+
+            <button onClick={this.AffP} className='cell'>{this.state.InValue}</button>
         );
     }
 }
