af533c0fbc858135f0b06286da4552486765a58d
dcorroyer <dylan.corroyer@wanadoo.fr>
2019-02-05 16:54:08 +0100

    Etape 6

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+describe('App tests', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<Case />);
+  });
+
+  describe('isCaseFull', () => {
+    it('Should return false at startup', () => {
+      expect(wrapper.instance().isCaseFull()).toBe(false);
+    });
+  });
+
+  describe('getWinner', () => {
+    it('Should return false at startup', () => {
+        expect(wrapper.instance.getWinner()).toBe(null);
+    });
+
+    it('Should return true is a player win', () => {
+        expect(wrapper.instance.getWinner()).toBe(true);
+    });
+  });
+
 });
