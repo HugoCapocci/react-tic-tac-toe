029b0674a85181d647f0d4672eee7a56ffe19108
voiku45 <34510578+voiku45@users.noreply.github.com>
2019-02-05 20:19:02 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Board from './Board.jsx';
 import './App.css';
 
 class App extends Component {
+  
+  constructor(){
+    super();
+    this.state ={
+      turn: 'X',
+      gameEnded: false,
+      board : Array(9).fill('')
+    }
+  }
+
+  clicked(event){
+
+    if(this.state.board[event.target.dataset.square] == ''){
+      this.state.board[event.target.dataset.square] = this.state.turn;
+      event.target.innerText = this.state.turn;
+      this.setState({
+        turn: this.state.turn ='X' ? 'O' : 'X',
+        board : this.state.board
+      })
+    }
+    console.log(this.state.board);
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div id="game">
+        <div id="head">
+        </div>
+        <div id="board" onClick={(e)=>this.clicked(e)}>
+          <div className="square" data-square="0"></div>
+          <div className="square" data-square="1"></div>
+          <div className="square" data-square="2"></div>
+          <div className="square" data-square="3"></div>
+          <div className="square" data-square="4"></div>
+          <div className="square" data-square="5"></div>
+          <div className="square" data-square="6"></div>
+          <div className="square" data-square="7"></div>
+          <div className="square" data-square="8"></div>
+        </div>
       </div>
     );
   }
