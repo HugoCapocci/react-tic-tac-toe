937b015e505d489ab43528fbf7416ac9abed266f
tiagfernandes <tiago_fernandes@live.fr>
2019-02-05 14:29:57 +0100

    Etape 3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Square from './Square';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h3>Next player : X</h3>
+          <div>
+            <Square index='0' />
+            <Square index='1' />
+            <Square index='2' />
+          </div>
+          <div>
+            <Square index='3' />
+            <Square index='4' />
+            <Square index='5' />
+          </div>
+          <div>
+            <Square index='6' />
+            <Square index='7' />
+            <Square index='8' />
+          </div>
         </header>
       </div>
     );
---
+++ src/Square.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Square extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
+  render() {
+    return (
+      <button className="square" onClick={() => this.setState({ value: 'X' })}>
+        {this.state.value}
+      </button>
+    );
+  }
+}
+
+export default Square;
