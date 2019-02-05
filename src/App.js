d61b2b606d007762aa233fb16e9683f8e02a3773
dcorroyer <dylan.corroyer@wanadoo.fr>
2019-02-05 13:53:46 +0100

    Première étape

--- src/App.js
+++ src/App.js
@@ -7,18 +7,25 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+
+          <div>
+            <button className="cell"></button>
+            <button className="cell"></button>
+            <button className="cell"></button>
+          </div>
+          
+          <div>
+            <button className="cell"></button>
+            <button className="cell"></button>
+            <button className="cell"></button>
+          </div>
+
+          <div>
+            <button className="cell"></button>
+            <button className="cell"></button>
+            <button className="cell"></button>
+          </div>
+
         </header>
       </div>
     );
