02eb8f1beab0ae751e799a77cb67b91f5ba4e7ff
Krusherr <32904340+Krusherr@users.noreply.github.com>
2019-02-05 15:04:00 +0100

    quatrieme etape

--- src/Case.js
+++ src/Case.js
@@ -4,20 +4,11 @@ import './App.css';
 
 
 export default class Case extends Component {
-    state={
-        'valueButton': null,
-    }
-    
-    handleClick = ()  => {
-        this.setState({'valueButton' :'X'} )
-      }
-    
- 
+  
   render() {
      
-     // this.setState({'valueButton' : this.props.value} )
     return (
-        <button className="cell" onClick={ this.handleClick }> { this.state.valueButton }</button>
+        <button className="cell" onClick={ this.props.handleClick }> { this.props.value }</button>
     );
   }
 }
--- src/Plateau.js
+++ src/Plateau.js
@@ -5,26 +5,44 @@ import './App.css';
 
 
 export default class Plateau extends Component {
+    state={
+      cells : [
+          null,  'X', 'X',
+          null, 'X', null,
+          null, null, 'X'
+      ],
+    }
+    handleClick = (index)  => {
+            this.setState(prevState =>{
+                const cells= [...prevState.cells];
+                cells[index]= 'X';
+                return {
+                    cells
+                }
+            })
+      }
   render() {
+       
+      
     return (
         <div className="App">
         
             <header className="App-header">
                 <h2>Next player : X</h2>    
                 <div >
-                    < Case value="0" />
-                    < Case value="1" />
-                    < Case value="2" />
+                    < Case value={ this.state.cells[0] } handleClick={ () => this.handleClick(0) } />
+                    < Case value={ this.state.cells[1] } handleClick={ () => this.handleClick(1) }  />
+                    < Case value={ this.state.cells[2] } handleClick={ () => this.handleClick(2) } />
                 </div>
                 <div >
-                    < Case value="3" />
-                    < Case value="4" />
-                    < Case value="5" />
+                    < Case value={ this.state.cells[3] } handleClick={ () => this.handleClick(3) } />
+                    < Case value={ this.state.cells[4] } handleClick={ () => this.handleClick(4) } />
+                    < Case value={ this.state.cells[5] } handleClick={ () => this.handleClick(5) } />
                 </div>
                 <div >
-                    < Case value="6" />
-                    < Case value="7" />
-                    < Case value="8" />
+                    < Case value={ this.state.cells[6] } handleClick={ () => this.handleClick(6) } />
+                    < Case value={ this.state.cells[7] } handleClick={ () => this.handleClick(7) } />
+                    < Case value={ this.state.cells[8] } handleClick={ () => this.handleClick(8) } />
                 </div>
             </header>
       </div>
