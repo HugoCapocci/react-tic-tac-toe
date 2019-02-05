9d64faa8fd2fd5e66720c0359d552eca66bd4ec1
ctritta <ctritta@timsoft.com>
2019-02-05 16:23:36 +0100

    step 5

--- src/Board.jsx
+++ src/Board.jsx
@@ -7,6 +7,7 @@ export default class Board extends Component {
         super(props);
         this.state = {
             cells: Array(9).fill(null),
+            xIsNext: true,
         };
     }
 
@@ -17,12 +18,44 @@ export default class Board extends Component {
 
     handleClick(i) {
         const cells = this.state.cells.slice();
-        cells[i] = 'X';
-        this.setState({cells: cells});
+        if (this.calculateWinner(cells) || cells[i])
+            return;
+        cells[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            cells: cells,
+            xIsNext: !this.state.xIsNext
+        });
+    }
+
+     calculateWinner(squares) {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
     }
 
     render() {
-        const status = 'Next player: X';
+        const winner = this.calculateWinner(this.state.cells);
+        var status;
+
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
 
         return (
             <div>
