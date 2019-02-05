8e328f65474f1c7723248f0a83601154cf50d1eb
ctritta <ctritta@timsoft.com>
2019-02-05 14:41:47 +0100

    step 3

--- src/Cell.js
+++ src/Cell.js
@@ -2,10 +2,18 @@ import React, { Component } from 'react';
 
 
 export default class Cell extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+
     render() {
         return (
-            <button className="cell">
-                {this.props.value}
+            <button className="cell"  onClick={() => this.setState({value: 'X'})}>
+                {this.state.value}
             </button>
         );
     }
