c7a5f2a748d746256858c8a0a74b6dcf80fc3769
Adrien Haye <adrihaye@gmail.com>
2019-02-05 15:51:23 +0100

    Alternance des joueurs

--- src/Square.jsx
+++ src/Square.jsx
@@ -2,20 +2,10 @@ import React, { Component } from 'react';
 
 
 export default class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-
   render() {
     return (
-      <button
-        className="square"
-        onClick={() => this.setState({value: 'X'})}
-      >
-        {this.state.value}
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
