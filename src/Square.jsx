e1a812dc153bd9050c58aa67acfbca6e7b0079cc
Adrien Haye <adrihaye@gmail.com>
2019-02-05 14:40:05 +0100

    Passage des valeurs entre components
    Les valeurs contenues dans les squares passent dans le board

--- src/Square.jsx
+++ src/Square.jsx
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 
 
-export default class Square extends Component {
+export default class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button
+        className="square"
+        onClick={() => this.setState({value: 'X'})}
+      >
+        {this.state.value}
       </button>
     );
   }
