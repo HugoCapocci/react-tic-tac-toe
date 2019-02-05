049c1d8895b53de9b7b3a5d59c8189abc77abe6d
Adrien Haye <adrihaye@gmail.com>
2019-02-05 16:54:43 +0100

    work in progress ...

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import Game from './Game';
 
-
 import './App.css';
 
 class App extends Component {
   render() {
+    
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+    
     return (
       <div className="App">
         <header className="App-header">
