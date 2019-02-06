71f87835d88ee09d73f70f5508e9920d90cc0402
Adrien Haye <adrihaye@gmail.com>
2019-02-06 11:04:30 +0100

    Its aliiiiive !
    Réparation du script :
    - La méthode calculateWinner est dans son propre fichier pour pouvoir être appelée n'importe quand.
    - Modif de l'appel à la méthode dans le Board plutôt que dans l'app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React, { Component } from 'react';
 import Game from './Game';
+import Board from './Board';
 
 import './App.css';
 
 class App extends Component {
   render() {
-    
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
     
     return (
       <div className="App">
