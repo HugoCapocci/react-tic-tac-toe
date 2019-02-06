265f8b129b350b13dee2fd5713385c3d50944123
NicoSar <nicolas.sarrazin88@gmail.com>
2019-02-06 13:50:35 +0100

    jeux fonctionnel + mode jour/nuit

--- src/App.js
+++ src/App.js
@@ -6,27 +6,125 @@ import Cell from './Cell';
 
 class App extends Component {
   constructor(props) {
-    super(props);
-    this.state = {
-      history: [{
-        squares: Array(9).fill(null),
-      }],
-      xIsNext: true,
-    };
-  }
-
-  render() {
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-          <div className="game-info">
-
-          </div>
-        </div>
-      );
+      super(props);
+      this.state = {
+        history: [{
+          squares: Array(9).fill(null),
+        }],
+        stepNumber: 0,
+        xIsNext: true,
+        stylePath: './App.css',
+        cssMode: 'Night'
+      };
     }
+
+    handleButtonClick(){
+      if(this.state.stylePath == './App2.css'){
+        this.setState({stylePath: './App.css'});
+        this.setState({cssMode: 'Night'});
+      }else{
+          this.setState({stylePath: './App2.css'});
+          this.setState({cssMode: 'Light'});
+      }
+
+
+    }
+
+    calculateWinner(squares) {
+     const lines = [
+       [0, 1, 2],
+       [3, 4, 5],
+       [6, 7, 8],
+       [0, 3, 6],
+       [1, 4, 7],
+       [2, 5, 8],
+       [0, 4, 8],
+       [2, 4, 6],
+     ];
+     for (let i = 0; i < lines.length; i++) {
+       const [a, b, c] = lines[i];
+       if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+         return squares[a];
+       }
+     }
+     return null;
+   }
+    handleClick(i) {
+       const history = this.state.history.slice(0, this.state.stepNumber + 1);
+      const current = history[history.length - 1];
+      const squares = current.squares.slice();
+      if (this.calculateWinner(squares) || squares[i]) {
+        return;
+      }
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+        history: history.concat([{
+          squares: squares,
+        }]),
+        stepNumber: history.length,
+        xIsNext: !this.state.xIsNext,
+      });
+    }
+
+    jumpTo(step) {
+  this.setState({
+    stepNumber: step,
+    xIsNext: (step % 2) === 0,
+  });
+}
+
+    render() {
+   const history = this.state.history;
+   const current = history[this.state.stepNumber];
+   const winner = this.calculateWinner(current.squares);
+
+   const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li key={move}>
+         <button onClick={() => this.jumpTo(move)}>{desc}</button>
+       </li>
+      );
+    });
+
+   let status;
+   if (winner) {
+
+     status = 'Winner: ' + winner;
+   } else {
+     status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+   }
+
+   return (
+
+
+
+
+     <div className="game">
+   <h1>Tic Tac Toe</h1>
+       <div className="game-board">
+         <Board
+           squares={current.squares}
+           onClick={(i) => this.handleClick(i)}
+         />
+
+       </div>
+       <div className="game-night">
+       <link rel="stylesheet" type="text/css" href={this.state.stylePath} />
+       <button type="button" onClick={this.handleButtonClick.bind(this)}>{this.state.cssMode} mode</button>
+       </div>
+       <div className="game-info">
+         <div>{status}</div>
+         <ol>{moves}</ol>
+       </div>
+
+     </div>
+
+
+   );
+ }
   }
 
 export default App;
