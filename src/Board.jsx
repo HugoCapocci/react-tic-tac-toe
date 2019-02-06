501bc9274632a1b9a6c60f0e0dde3635a13a0f54
ctritta <ctritta@timsoft.com>
2019-02-06 11:54:04 +0100

    reformat code

--- src/Board.jsx
+++ src/Board.jsx
@@ -3,65 +3,17 @@ import Cell from './Cell';
 
 export default class Board extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            cells: Array(9).fill(null),
-            xIsNext: true,
-        };
-    }
-
 
     renderCell(i) {
-        return <Cell value={this.state.cells[i]}  onClick={() => this.handleClick(i)}/>;
+        return <Cell value={this.props.cells[i]}  onClick={() => this.props.onClick(i)}/>;
     }
 
-    handleClick(i) {
-        const cells = this.state.cells.slice();
-        if (this.calculateWinner(cells) || cells[i])
-            return;
-        cells[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-            cells: cells,
-            xIsNext: !this.state.xIsNext
-        });
-    }
 
-     calculateWinner(squares) {
-        const lines = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [0, 4, 8],
-            [2, 4, 6],
-        ];
-        for (let i = 0; i < lines.length; i++) {
-            const [a, b, c] = lines[i];
-            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-                return squares[a];
-            }
-        }
-        return null;
-    }
 
     render() {
-        const winner = this.calculateWinner(this.state.cells);
-        var status;
-
-        if (winner) {
-            status = 'Winner: ' + winner;
-        } else {
-            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-        }
 
         return (
             <div>
-                <div className="status">
-                    {status}
-                </div>
 
             <table>
                 <tbody>
--- src/Game.jsx
+++ src/Game.jsx
@@ -3,15 +3,74 @@ import './App.css';
 import Board from './Board'
 
 class Game extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            history: [{
+                cells: Array(9).fill(null),
+            }],
+            xIsNext: true,
+        };
+    }
+
+    handleClick(i) {
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const cells = current.cells.slice();
+        if (this.calculateWinner(cells) || cells[i])
+            return;
+        cells[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            history: history.concat([{
+                cells: cells,
+            }]),
+            xIsNext: !this.state.xIsNext,
+        });
+    }
+
+
+    calculateWinner(squares) {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
+    }
+
   render() {
-    return (
+      const history = this.state.history;
+      const current = history[history.length - 1];
+      const winner = this.calculateWinner(current.cells);
+
+      let status;
+      if (winner) {
+          status = 'Winner: ' + winner;
+      } else {
+          status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
+
+
+      return (
         <div className="game">
           <div className="game-board">
-            <Board />
+            <Board cells={current.cells}
+                    onClick={(i) => this.handleClick(i)}/>
           </div>
           <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
+                <div>{status}</div>
           </div>
         </div>
     );
